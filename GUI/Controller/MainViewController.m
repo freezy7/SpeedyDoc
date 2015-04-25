0d2f3e62a33f754820a1055fc3566d5108d7d6c8
freezy7 <453901962@qq.com>
2015-04-25 22:16:32 +0800

    repair XLForm 2.2.0 error

--- GUI/Controller/MainViewController.m
+++ GUI/Controller/MainViewController.m
@@ -5,7 +5,7 @@
 //  Created by R_style_Man on 15/4/25.
 //  Copyright (c) 2015年 R_style Man. All rights reserved.
 //
-
+#import "XLForm.h"
 #import "MainViewController.h"
 
 @interface MainViewController ()
@@ -14,24 +14,119 @@
 
 @implementation MainViewController
 
-- (void)viewDidLoad {
+NSString *const kName = @"name";
+NSString *const kEmail = @"email";
+NSString *const kTwitter = @"twitter";
+NSString *const kNumber = @"number";
+NSString *const kInteger = @"integer";
+NSString *const kDecimal = @"decimal";
+NSString *const kPassword = @"password";
+NSString *const kPhone = @"phone";
+NSString *const kUrl = @"url";
+NSString *const kTextView = @"textView";
+NSString *const kNotes = @"notes";
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self)
+    {
+        self = [self init];
+    }
+    return self;
+}
+
+-(id)init
+{
+    XLFormDescriptor * formDescriptor = [XLFormDescriptor formDescriptorWithTitle:@"Text Fields"];
+    XLFormSectionDescriptor * section;
+    XLFormRowDescriptor * row;
+    
+    formDescriptor.assignFirstResponderOnShow = YES;
+    
+    // Basic Information - Section
+    section = [XLFormSectionDescriptor formSectionWithTitle:@"TextField Types"];
+    section.footerTitle = @"This is a long text that will appear on section footer";
+    [formDescriptor addFormSection:section];
+    
+    // Name
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kName rowType:XLFormRowDescriptorTypeText title:@"Name"];
+    row.required = YES;
+    [section addFormRow:row];
+    
+    // Email
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kEmail rowType:XLFormRowDescriptorTypeEmail title:@"Email"];
+    // validate the email
+    [row addValidator:[XLFormValidator emailValidator]];
+    [section addFormRow:row];
+    
+    // Twitter
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kTwitter rowType:XLFormRowDescriptorTypeTwitter title:@"Twitter"];
+    row.disabled = YES;
+    row.value = @"@no_editable";
+    [section addFormRow:row];
+    
+    // Number
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kNumber rowType:XLFormRowDescriptorTypeNumber title:@"Number"];
+    [section addFormRow:row];
+    
+    // Integer
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kInteger rowType:XLFormRowDescriptorTypeInteger title:@"Integer"];
+    [section addFormRow:row];
+    
+    // Decimal
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kDecimal rowType:XLFormRowDescriptorTypeDecimal title:@"Decimal"];
+    [section addFormRow:row];
+    
+    // Password
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kPassword rowType:XLFormRowDescriptorTypePassword title:@"Password"];
+    [section addFormRow:row];
+    
+    // Phone
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kPhone rowType:XLFormRowDescriptorTypePhone title:@"Phone"];
+    [section addFormRow:row];
+    
+    // Url
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kUrl rowType:XLFormRowDescriptorTypeURL title:@"Url"];
+    [section addFormRow:row];
+    
+    
+    section = [XLFormSectionDescriptor formSection];
+    [formDescriptor addFormSection:section];
+    
+    
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kTextView rowType:XLFormRowDescriptorTypeTextView];
+    [row.cellConfigAtConfigure setObject:@"TEXT VIEW EXAMPLE" forKey:@"textView.placeholder"];
+    [section addFormRow:row];
+    
+    section = [XLFormSectionDescriptor formSectionWithTitle:@"TextView With Label Example"];
+    [formDescriptor addFormSection:section];
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:kNotes rowType:XLFormRowDescriptorTypeTextView title:@"Notes"];
+    [section addFormRow:row];
+    
+    return [super initWithForm:formDescriptor];
+    
+}
+
+-(void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(savePressed:)];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(IBAction)savePressed:(UIBarButtonItem * __unused)button
+{
+    NSArray * validationErrors = [self formValidationErrors];
+    if (validationErrors.count > 0){
+        [self showFormValidationError:[validationErrors firstObject]];
+        return;
+    }
+    [self.tableView endEditing:YES];
+    NSDictionary* data = [self formValues];
+    NSLog(@"data = %@",data);
+    UIAlertView * alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Valid Form", nil) message:@"No errors found" delegate:self cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil];
+    [alertView show];
 }
-*/
 
 @end
