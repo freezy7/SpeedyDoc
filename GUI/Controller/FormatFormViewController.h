efe8e96388ae5565e43f03a164c2b8db46d71501
freezy7 <453901962@qq.com>
2015-06-15 16:31:11 +0800

    tiny fix

---
+++ GUI/Controller/FormatFormViewController.h
@@ -0,0 +1,13 @@
+//
+//  FormatFormViewController.h
+//  SpeedyDoc
+//
+//  Created by R_style Man on 15/6/15.
+//  Copyright (c) 2015年 R_style Man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FormatFormViewController : UITableViewController
+
+@end
